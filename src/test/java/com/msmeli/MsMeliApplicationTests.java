b086ddd379c781511d6ece80dcc3de3b551a9c83
Octavio <oheuman@gmail.com>
2023-08-29 12:05:29 -0300

    Repositorio Creado

---
+++ src/test/java/com/msmeli/MsMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msmeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
