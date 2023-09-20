fc43419307373619a3774e1e76302ee656950407
Franco Bussano <franbussano@gmail.com>
2023-09-20 15:50:08 -0300

    CatalogItemResponseDTO created

--- src/main/java/com/msmeli/dto/response/CatalogItemResponseDTO.java
+++ src/main/java/com/msmeli/dto/response/CatalogItemResponseDTO.java
@@ -1,8 +1,17 @@
 package com.msmeli.dto.response;
 
+import java.util.Date;
+
 public class CatalogItemResponseDTO {
 
+    private Double price;
+    private String catalog_product_id;
     private String nickname;
+    private int sold_quantity;
+    private int available_quantity;
+    private String category_id;
+    private Date created_date_item;
+    private Date updated_date_item;
 
 
 }
