8bbf2a81f41581e5060bf8c8606e9fb157472e5f
SimonS89 <simonrsueldo@gmail.com>
2023-10-27 13:23:33 -0300

    fix null cost columns

--- src/main/java/com/msmeli/repository/StockRepository.java
+++ src/main/java/com/msmeli/repository/StockRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Integer> {
-    @Query("SELECT s FROM Stock s WHERE s.sku = ?1 AND s.register_date = (SELECT MIN(s2.register_date) FROM Stock s2 WHERE s2.sku = ?1) ORDER BY s.register_date LIMIT 1")
+//    @Query("SELECT s FROM Stock s WHERE s.sku = ?1 AND s.register_date = (SELECT MIN(s2.register_date) FROM Stock s2 WHERE s2.sku = ?1) ORDER BY s.register_date LIMIT 1")
+    @Query("SELECT s FROM Stock s WHERE s.sku = ?1 ORDER BY s.register_date LIMIT 1")
     Stock findBySku(String sku);
 
     @Query("SELECT sum(s.available_quantity) FROM Stock s WHERE s.sku = ?1")
--- src/main/java/com/msmeli/service/implement/CostServiceImpl.java
+++ src/main/java/com/msmeli/service/implement/CostServiceImpl.java
@@ -30,26 +30,27 @@ public class CostServiceImpl implements CostService {
     public Item createProductsCosts(Item item, Stock stock) {
         Cost cost = mapper.map(item, Cost.class);
         FeeRequestDTO feeRequestDTO = new FeeRequestDTO();
+        Double shippingCost = 0.0;
+        FeeDetailsDTO feeDetails = null;
         try {
             cost.setId(item.getId());
             feeRequestDTO.setCategoryId(item.getCategory_id());
             feeRequestDTO.setListingTypeId(item.getListing_type_id());
             feeRequestDTO.setPrice(item.getPrice());
-            FeeDetailsDTO feeDetails = meliService.getItemFee(item.getPrice(), item.getCategory_id(), item.getListing_type_id()).getSale_fee_details();
+            feeDetails = meliService.getItemFee(item.getPrice(), item.getCategory_id(), item.getListing_type_id()).getSale_fee_details();
+            shippingCost = meliService.getShippingCostDTO(item.getId()).getOptions().stream().filter(option -> option.getName().equals("Estándar a sucursal de correo")).findFirst().get().getBase_cost();
+        } catch (FeignException.NotFound | FeignException.InternalServerError ignored) {
+        } finally {
+            if (item.getSku() != null && stock != null) cost.setReplacement_cost(stock.getPrice());
             cost.setComision_fee(feeDetails.getPercentage_fee());
             cost.setComision_discount(feeDetails.getGross_amount());
-            if (item.getSku() != null && stock != null) cost.setReplacement_cost(stock.getPrice());
-            Double shippingCost = meliService.getShippingCostDTO(item.getId()).getOptions().stream().filter(option -> option.getName().equals("Estándar a sucursal de correo")).findFirst().get().getBase_cost();
             cost.setShipping(shippingCost);
             double total_cost = item.getPrice() * GrossIncome.IIBB.iibPercentage + (cost.getComision_discount() + cost.getShipping() + cost.getReplacement_cost());
             cost.setTotal_cost(total_cost);
-            double margin = (item.getPrice() - total_cost) *100/item.getPrice();
+            double margin = (item.getPrice() - total_cost) * 100 / item.getPrice();
             cost.setMargin(margin);
             double profit = item.getPrice() - total_cost;
             cost.setProfit(profit);
-        } catch (FeignException.NotFound | FeignException.InternalServerError ignored) {
-            cost.setShipping(0);
-        } finally {
             item.setCost(costRepository.save(cost));
             return item;
         }
