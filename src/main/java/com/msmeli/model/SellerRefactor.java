12d3558eb9e8637472cfd63195b5791a11ce8299
Fran! <francisconu1125@gmail.com>
2023-11-28 14:02:59 -0300

    arreglo de mapeo en base de dato Sellers

--- src/main/java/com/msmeli/model/SellerRefactor.java
+++ src/main/java/com/msmeli/model/SellerRefactor.java
@@ -13,7 +13,9 @@ import java.util.List;
 @NoArgsConstructor
 @Table(name = "sellers")
 public class SellerRefactor extends UserEntity {
-    private String meliId;
+    private String meliID;
+    private String refreshToken;
+    private String tokenMl;
     @OneToMany(mappedBy = "sellerRefactor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Employee> employees;
 }
